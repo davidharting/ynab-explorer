fad5f59e002ad534a61e81fcdd00322fa26ebb77
David Harting <davideharting@gmail.com>
2018-09-16 11:51:01 -0400

    🗂 Update model for new category column

--- models/transaction.py
+++ models/transaction.py
@@ -9,7 +9,8 @@ class Transaction(Base):
   id = Column(String, primary_key = True)
   date = Column(DateTime)
   amount = Column(Numeric)
+  category = Column(String)
   payee = Column(String)
 
   def __repr__(self):
-    return '<Transaction date={} amount={} payee={}>'.format(self.date, self.amount, self.payee)
+    return '<Transaction date={} amount={} category={} payee={}>'.format(self.date, self.amount, self.category, self.payee)
