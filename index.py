1e55ccbe736c7c685a141420396e929a354cd584
David Harting <davideharting@gmail.com>
2018-09-16 11:55:43 -0400

    ♻️ Close session. Use try block.

--- index.py
+++ index.py
@@ -11,13 +11,21 @@ def main():
   # client = YnabClient(token)
   # budgets = client.getBudgets()
   # print(budgets)
+
   engine = create_engine(connection_url)
   Session = sessionmaker()
   Session.configure(bind=engine)
   session = Session()
-  transaction = Transaction(id=uuid4(), date=datetime.today(), amount=37.87, payee='Target')
-  session.add(transaction)
-  session.commit()
+
+  try:
+    transaction = Transaction(id=uuid4(), date=datetime.today(), amount=37.87, category='Dining Out', payee='Target')
+    session.add(transaction)
+    session.commit()
+  except:
+    session.rollback()
+    raise
+  finally:
+    session.close()
    
 if __name__ == '__main__':
   main()
